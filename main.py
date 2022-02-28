fdd9713caf31f1ff4d6721a929f30ddcd1575559
anton <antonplay983@gmail.com>
2022-02-28 22:36:31 +0300

    testupdate

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import pygame
 
 
+
+
+
 def main():
-    print('зadffasfо')
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 1000))
+    clock = pygame.time.Clock()
+    clock.tick(60)
+    pygame.display.set_caption("My Game")
+    screen.fill(BLACK)
+    pygame.display.flip()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                print(run)
+                return
 
 
 if __name__ == "__main__":
