1b16bb1d30c2890a2a3681aefcd1934599ba7fc1
anton <antonplay983@gmail.com>
2022-03-01 22:29:24 +0300

    Игра alpha

--- ray_casting.py
+++ ray_casting.py
@@ -11,6 +11,7 @@ def ray_casting(sc, player_pos, player_angle):
         for depth in range(max_depth):
             x = x0 + depth * cos_a
             y = y0 + depth * sin_a
+
             # pygame.draw.line(sc, (10, 10, 10), player_pos, (x, y), 2)
             if (x // tile * tile, y // tile * tile) in world_map:
                 depth *= math.cos(player_angle - cur_angle)
