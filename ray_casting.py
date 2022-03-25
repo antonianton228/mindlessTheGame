486b94f5580e4bb0da009992e16c50070ceab1aa
antonianton228 <58910703+antonianton228@users.noreply.github.com>
2022-03-25 21:52:47 +0300

    Update ray_casting.py

--- ray_casting.py
+++ ray_casting.py
@@ -1,10 +1,10 @@
 import pygame
 from settings import *
-from maps import world_map, world_width, world_height
+import maps
 from numba import njit
 import numpy as np
 
-
+world_map, world_width, world_height = maps.map_call()[1], maps.map_call()[2], maps.map_call()[3]
 
 @njit(fastmath=True)
 def mapping(a, b):
