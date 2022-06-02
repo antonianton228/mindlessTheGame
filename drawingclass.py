e5290d13e8260762de0ba759bc31fe9d7850f826
Ishak <vesilgame2006@gmail.com>
2022-06-02 19:40:59 +0300

    с полом 2

--- drawingclass.py
+++ drawingclass.py
@@ -6,6 +6,9 @@ from settings import *
 from ray_casting import ray_casting
 from collections import deque
 from weaponClass import Weapon
+import pygame as pg
+import numpy as np
+from numba import njit
 
 
 class Drawing:
@@ -193,6 +196,31 @@ class Drawing:
             pygame.display.flip()
             self.clock.tick(100)
 
+    def floor(self, posx, posy, rot, move, surf, s):
+        if move and s < 2:
+            hres = 120  # horizontal resolution
+            halfvres = 70  # vertical resolution/2
+
+            mod = hres / 60  # scaling factor (60° fov)
+            frame = np.random.uniform(0, 1, (hres, halfvres * 2, 3))
+            sky = pg.image.load('data/textures/skybox.jpg')
+            sky = pg.surfarray.array3d(pg.transform.scale(sky, (360, halfvres * 2))) / 255
+            floor = pg.surfarray.array3d(pg.image.load('data/textures/floor.jpg')) / 255
+
+            frame = new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod)
+
+            surf = pg.surfarray.make_surface(frame * 255)
+            surf = pg.transform.scale(surf, (1200, 800))
+
+        return 100000, surf, (0, 0)
+
+    def floor_settings(self, x, y, angle, last_x, last_y, last_angle, s, move):
+        if x == last_x and y == last_y and angle == last_angle:
+            move = 0
+        else:
+            s = s + 1 if s < 4 else 0
+        return move, s
+
 
 class Button:
     def __init__(self, text, x, y, width, height, command=None):
@@ -243,3 +271,18 @@ class Button:
                 return True
                 # if self.command:
                 #     self.command()
+
+@njit()
+def new_frame(posx, posy, rot, frame, sky, floor, hres, halfvres, mod):
+    posy, posx = posy * 0.01, posx * 0.01
+    for i in range(hres):
+        rot_i = rot + np.deg2rad(i / mod - 30)
+        sin, cos, cos2 = np.sin(rot_i), np.cos(rot_i), np.cos(np.deg2rad(i / mod - 30))
+        frame[i][:] = sky[int(np.rad2deg(rot_i) % 359)][:]
+        for j in range(halfvres):
+            n = (halfvres / (halfvres - j)) / cos2
+            x, y = posx + cos * n, posy + sin * n
+            xx, yy = int(x * 2 % 1 * 99), int(y * 2 % 1 * 99)
+            frame[i][halfvres * 2 - j - 1] = floor[xx][yy]
+
+    return frame
--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from ray_casting import ray_casting_walls
 from npc_ai import Interaction
 import storyteller
 import pygame as pg
-import floor
 
 
 
@@ -33,21 +32,20 @@ interaction.play_music()
 while True:
     world_map = maps.map_call()[1]
     flagloop = True
-    last_surf = []
+    floor_and_sky = [0, 0, 0]
+    s = 0
     while flagloop:
         last_x, last_y, last_angle, move = player.x, player.y, player.angle, 1000000
         player.movement()
-        if player.x == last_x and player.y == last_y and player.angle == last_angle:
-            move = 0
+        move, s = drawing.floor_settings(player.x, player.y, player.angle, last_x, last_y, last_angle, s, move)
 
         walls, wall_hit = ray_casting_walls(player, drawing.textures)
 
         sprites.list_of_objects = sprites.dict_of_objects[storyteller.get_level()]
 
         # drawing.floor_drow(sc)
-        floor_sky = floor.floor(player.x, player.y, player.angle, move, last_surf)
-        last_surf = floor_sky[1]
-        drawing.world(walls + [obj.object_locate(player) for obj in sprites.list_of_objects] + [floor_sky])
+        floor_and_sky = drawing.floor(player.x, player.y, player.angle, move, floor_and_sky[1], s)
+        drawing.world(walls + [obj.object_locate(player) for obj in sprites.list_of_objects] + [floor_and_sky])
         drawing.fps(clock)
         drawing.player_weapon([wall_hit, sprites.sprite_hit])
 
